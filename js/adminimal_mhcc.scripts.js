3b8363d8dd2a85726adc301fd551837eadccdaeb
palagpatski <palagpatski@gmail.com>
2021-04-27 13:02:23 +0800

    Move role expire fields inside their corresponding role fields

--- js/adminimal_mhcc.scripts.js
+++ js/adminimal_mhcc.scripts.js
@@ -4,6 +4,9 @@
    */
   Drupal.behaviors.roleFieldsRelo = {
     attach: function(context, settings) {
+      $('[class*="form-item-roles"]').each(function() {
+        $(this).next('[class*="form-item-role-expire"]').appendTo($(this));
+      });
       if (!$('#edit-roles--wrapper, #edit-status--wrapper').length) {
         $('#status-and-roles').hide();
       } else {
